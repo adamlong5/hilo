c757bc147223e2a6279722fcc7ecad3f0c27a29d
Adam Long <adamblong1103@gmail.com>
2016-12-29 14:24:37 -0500

    react repo setup

---
+++ test/helpers.js
@@ -0,0 +1,30 @@
+import { jsdom } from 'jsdom';
+import chai from 'chai';
+const { expect } = chai;
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import chaiEnzyme from 'chai-enzyme';
+chai.use(chaiEnzyme());
+
+global.expect = expect;
+global.shallow = shallow;
+global.mount = mount;
+global.render = render;
+global.React = React;
+
+const exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+global.documentRef = document;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const path = require('path');                // a useful node path helper library
+
+const config = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/main.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'), // store the bundled output in dist/bundle.js
+    filename: 'bundle.js'                  // specifying file name for our compiled assets
+  },
+  module: {
+    loaders: [
+      // telling webpack which loaders we want to use.  For now just run the
+      // code through the babel-loader.  'babel' is an alias for babel-loader
+      { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ }
+    ]
+  }
+}
+
+module.exports = config;
