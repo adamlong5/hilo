dc0662b59838c38055353dce3e848df9257b3809
Adam Long <adamblong1103@gmail.com>
2016-12-30 09:47:16 -0500

    disable buttons when deck runs out

--- src/components/PlayControl.js
+++ src/components/PlayControl.js
@@ -3,21 +3,29 @@ import { connect } from 'react-redux'
 import { drawCard, resetGame, swapPlayers } from '../actions/actions'
 
 export const PlayControlComponent = (props) => {
-  if (!props.drawPileLength) {
+  if (!props.drawPileLength && props.cardsRemainingInDeck) {
     props.drawCard(props.deckId)
     return null // Hide the controls
   }
+
   return (
     <div>
       <strong>Current Player</strong>: { `Player ${props.playingId + 1 || 1}` }
-      <button onClick={() => props.drawCard(props.deckId)}>Draw Card</button>
-      <button onClick={props.swapPlayers} disabled={props.drawPileLength < 4}>Swap Players</button>
+      <button
+        onClick={() => props.drawCard(props.deckId)}
+        disabled={props.cardsRemainingInDeck <= 0}
+      >Draw Card</button>
+      <button
+        onClick={props.swapPlayers}
+        disabled={props.drawPileLength < 4 || !props.cardsRemainingInDeck}
+      >Swap Players</button>
       <button onClick={props.resetGame}>Reset Game</button>
     </div>
   )
 }
 
 PlayControlComponent.propTypes = {
+  cardsRemainingInDeck: React.PropTypes.number.isRequired,
   deckId: React.PropTypes.string.isRequired,
   drawCard: React.PropTypes.func.isRequired,
   drawPileLength: React.PropTypes.number.isRequired,
@@ -26,10 +34,11 @@ PlayControlComponent.propTypes = {
   swapPlayers: React.PropTypes.func.isRequired,
 }
 
-const mapStateToProps = ({ drawPile, playingId, deckId }) => ({
+const mapStateToProps = ({ deckId, discardPile, drawPile, playingId }) => ({
   deckId,
   playingId,
   drawPileLength: drawPile.length || 0,
+  cardsRemainingInDeck: (52 - drawPile.length - discardPile.length) || 52,
 })
 const PlayControl = connect(
   mapStateToProps,
